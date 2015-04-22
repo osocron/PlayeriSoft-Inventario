428b483be80668d24eb1448693e9ccbfe1751f68
Noe Alejandro Perez Dominguez <osocron@gmail.com>
2015-04-22 18:02:48 -0500

    Avance

--- src/playeriSoft/controlador/Controller.java
+++ src/playeriSoft/controlador/Controller.java
@@ -1,4 +1,33 @@
 package playeriSoft.controlador;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import javax.swing.text.html.ListView;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable{
+
+    @FXML
+    private TextField searchTextView;
+    @FXML
+    private ListView prodListView;
+    @FXML
+    private Button nuevoButton;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+
+    }
+
+    public void abrirProductView(ActionEvent event){
+        
+    }
+
 }
