6196a1ae13b52d79c72745c7a6d2456431ceb957
Noe Aljenadro Perez Dominguez <osocron@gmail.com>
2015-04-23 19:06:25 +0200

    Resolved Controller Bug

--- src/playeriSoft/controlador/Controller.java
+++ src/playeriSoft/controlador/Controller.java
@@ -4,9 +4,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-
-import javax.swing.text.html.ListView;
 import java.net.URL;
 import java.util.ResourceBundle;
 
